8f2add7ca8203ee833a45df1b37ac667d012da8f
sunn kumar maurya <sunnymaurya2007@gmail.com>
2013-06-10 01:32:07 +0530

    made a rails application for taxibook

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taxibook::Application.config.secret_token = '96c56e7db4a00ebb18ff7b0de6031f4c54474e76fc7f09661f125542aa99b367963b12e0ed537dd81db52299758a84d1c67d6f6c38cb827ed188076ddf603fe9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Taxibook::Application.config.session_store :cookie_store, key: '_taxibook_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Taxibook::Application.config.session_store :active_record_store
